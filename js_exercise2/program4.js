faeed451e9ff3d9c20f4a86976f854f8b2848ddf
prashant.tomar <prashant.tomar@rapidops.com>
2020-02-14 18:33:28 +0530

    program 4 and 5 uploaded

---
+++ js_exercise2/program4.js
@@ -0,0 +1,12 @@
+function Suffle(){
+    arr=['a','b','c','d','e','f','g','h','i','j'];
+    n=arr.length;
+    let arrNew=[];
+    for(i=0;i<n;i++){
+        let n =arr.length;
+        r=Math.floor(Math.random()*n);
+        arrNew[i]=arr[r];
+        arr.splice(r,1);   
+    }
+    return(console.log(arrNew));
+}
---
+++ js_exercise2/program5.js
@@ -0,0 +1,20 @@
+arr1=['a','b','c','d','t','g','s','g','e','g','t','w'];
+arr2=['a','f','e','u','a','j','d','t','q','d','t','k'];
+arrUnion=(arr1+","+arr2).split(",");
+arrDiff1=[];
+arrDiff2=[];
+
+console.log(Array.from(new Set(arrUnion))) ;
+
+
+for(i=0;i<arr1.length;i++){
+    if(!arr2.includes(arr1[i]) && !arrDiff1.includes(arr1[i])){
+        arrDiff1.push(arr1[i]);
+    }
+}
+for(i=0;i<arr2.length;i++){
+    if(!arr1.includes(arr2[i]) && !arrDiff2.includes(arr2[i])){
+        arrDiff2.push(arr2[i]);
+    }
+}
+console.log(arrDiff1,arrDiff2);
