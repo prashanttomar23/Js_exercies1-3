38a31342f5e2a232018222f78437cf38fcd8f34e
prashant.tomar <prashant.tomar@rapidops.com>
2020-03-02 18:29:56 +0530

    task 3 of exercise3 modified

--- js_exercise3/task3/scriptBox.js
+++ js_exercise3/task3/scriptBox.js
@@ -1,57 +1,40 @@
 alert("Hello");
 let box3=document.getElementById("box1");
 box3.addEventListener("click",addText);
-let id=setInterval(colorChange,1000);
 arr=["green","blue","red"];
 arr2=["lightgreen","khaki","royalblue","purple","orangered"];
 let i=0;
 let j=0;
-function colorChange(){
+
+setInterval( function colorChange(){
     if(i>2){
         i=0;
     }
     document.getElementById("box2").style.backgroundColor=arr[i];
     i++;
-}
+}, 1000);
+
 function addText(){
     document.getElementById("box3").innerHTML='Oops something wrong?';
 }
 
-document.onkeydown=(function(event){
-     let keey=event.key;
-     if(keey=="ArrowRight" || keey=="ArrowUp"){
-        forward();
-    }
-    else if(keey=="ArrowLeft" || keey=="ArrowDown"){
-        backward();
-    }
-});
-
-
-
-function forward(){
-    let id=setInterval(function(){
-        
-        if(j>5){
-            j=0;
-            clearInterval(id);
+let keyy;
+let id;
+document.onkeydown=function(event){
+     keyy=event.key;
+     if(keyy=="ArrowRight" || keyy=="ArrowUp"){
+        document.getElementById("box4").style.backgroundColor=arr2[i];
+        if(i>5){
+            i=0;
         }
-        document.getElementById("box4").style.backgroundColor=arr2[j];
-        j++;
-    
-    },1000);
-    
-}
-function backward(){
-    let id=setInterval(function(){
-        clearInterval(id);
-        if(j<0){
-            j=5;
+        i++;
+    }
+    else if(keyy=="ArrowLeft" || keyy=="ArrowDown"){
+        document.getElementById("box4").style.backgroundColor=arr2[i];
+        if(i<0){
+            i=5;
         }
-        document.getElementById("box4").style.backgroundColor=arr2[j];
-        j--;
-
-    },1000)
-   
-
+        i--;
+    }
 }
+
